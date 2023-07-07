c578f4dfb55161752f250588215905472a849277
neel0patel <neelp072@gmail.com>
2023-07-07 17:55:25 -0400

    added additional components

--- Extended Course Work/playlist-generator/client/src/App.js
+++ Extended Course Work/playlist-generator/client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Home from "./home";
+import Loading from "./loading";
+import spotifyRecommender from "./spotifyRecommender";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    axios.get("/auth/current-session").then(({ data }) => {
+      setAuth(data);
+    });
+  }, []);
+
+  if (auth === null) {
+    return <Loading />;
+  }
+  if (auth) {
+    return <spotifyRecommender auth={auth} />;
+  }
+  return <Home />;
 }
 
 export default App;
---
+++ Extended Course Work/playlist-generator/client/src/spotifyRecommender.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const spotifyRecommender = () => {
+  return <div>Logged in !</div>;
+};
+
+export default spotifyRecommender;
